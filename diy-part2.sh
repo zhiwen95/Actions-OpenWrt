dc4cdba8ceda47241bba058f348cdf413bff9fc6
zhiwen <wei.zhiwen@outlook.com>
2021-07-05 13:05:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  lede/package/lean/luci-app-jd-dailybonus
