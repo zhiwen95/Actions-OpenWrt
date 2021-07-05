cf39f54436276760945fb7a30d3c58da29934ac7
zhiwen <wei.zhiwen@outlook.com>
2021-07-05 13:19:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
